b653199f1d3001def446e0fea5b35768f5e27027
kBarbz <kbarbz1995@gmail.com>
2019-11-14 15:14:50 +0800

    Set up skeleton for app

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
